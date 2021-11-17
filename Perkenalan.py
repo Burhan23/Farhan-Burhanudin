e60a268e42c117838b74e2bd80878405618baea0
Burhan23 <94538129+Burhan23@users.noreply.github.com>
2021-11-17 19:51:07 +0700

    Add files via upload

---
+++ Perkenalan.py
@@ -0,0 +1,11 @@
+Nama = 'Farhan Burhanudin'
+Nim = 2033
+Umur = '19 tahun'
+Tinggi_badan = '171'
+Berat_badan = 55
+
+print(Nama)
+print(Nim)
+print(Umur)
+print(Tinggi_badan, 'cm')
+print(Berat_badan, 'kg')
